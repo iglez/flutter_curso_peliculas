4b5345e3f54f4bdce068ceac5d791f4fd9c5723e
Ivan Gonzalez <ivan@iconos.mx>
2021-02-27 17:53:06 -0800

    Cambiando dimenciones

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -11,20 +11,22 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
     return Container(
       padding: EdgeInsets.only(top: 10.0),
       width: double.infinity,
-      height: 300.0,
+      height: _screenSize.height * 0.5,
       child: Swiper(
-        itemBuilder: (BuildContext context, int index){
+        itemBuilder: (BuildContext context, int index) {
           return Image.network("http://via.placeholder.com/350x150",
-            fit: BoxFit.fill);
+              fit: BoxFit.fill);
         },
         itemCount: 3,
         pagination: SwiperPagination(),
         control: SwiperControl(),
-        itemWidth: 200.0,
-        itemHeight: 200.0,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
         layout: SwiperLayout.STACK,
       ),
     );
