a8eb66fdea3b48569d009d52cd754ed10ee1e61f
Ivan Gonzalez <ivan@iconos.mx>
2021-02-27 20:13:54 -0800

    Creando Widget movie horizontal vacio

---
+++ lib/src/widgets/movie_horizontal_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class MovieHorizontal extends StatelessWidget {
+  // const MovieHorizontal({Key key}) : super(key: key);
+
+  final List<Pelicula> peliculas;
+
+  MovieHorizontal({@required this.peliculas});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
