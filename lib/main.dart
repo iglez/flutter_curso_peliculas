fe67ba15c3a97c6857d17398657b9e6ed0395986
Ivan Gonzalez <ivan@iconos.mx>
2021-02-27 17:12:50 -0800

    Crear home page y referenciando en ruta

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // mateapp
 import 'package:flutter/material.dart';
+
+import 'package:peliculas/src/pages/home_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -7,17 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: 'Peliculas',
+      initialRoute: '/',
+      routes: {
+        '/' : (BuildContext context) => HomePage(), 
+      },
     );
   }
 }
