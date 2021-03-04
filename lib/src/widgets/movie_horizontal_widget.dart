035dc080020fcd1ecaf90f60fa2acc6ad8c79551
Ivan Gonzalez <ivan@iconos.mx>
2021-03-03 19:19:02 -0800

    Agregando un page controller

--- lib/src/widgets/movie_horizontal_widget.dart
+++ lib/src/widgets/movie_horizontal_widget.dart
@@ -8,6 +8,10 @@ class MovieHorizontal extends StatelessWidget {
 
   MovieHorizontal({@required this.peliculas});
 
+  final _pageController = PageController(
+    initialPage: 1,
+    viewportFraction: 0.3);
+
   @override
   Widget build(BuildContext context) {
     final _sceenSize = MediaQuery.of(context).size;
@@ -16,10 +20,7 @@ class MovieHorizontal extends StatelessWidget {
       height: _sceenSize.height * 0.3,
       child: PageView(
         pageSnapping: false,
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3,
-        ),
+        controller: _pageController,
         children: _tarjetas(context),
       ),
     );
@@ -29,25 +30,25 @@ class MovieHorizontal extends StatelessWidget {
     return peliculas.map((pelicula) {
       return Container(
         margin: EdgeInsets.only(right: 5.0),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/img/no-image.jpg'),
-                image: NetworkImage(pelicula.getPosterImg()),
-                fit: BoxFit.cover,
-                height: 160.0,
-              ),
+        child: Column(children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              image: NetworkImage(pelicula.getPosterImg()),
+              fit: BoxFit.cover,
+              height: 160.0,
             ),
-            SizedBox(height: 5.0,),
-            Text(
-              pelicula.title,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
-            )
-          ]
-        ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            pelicula.title,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.caption,
+          )
+        ]),
       );
     }).toList();
   }
