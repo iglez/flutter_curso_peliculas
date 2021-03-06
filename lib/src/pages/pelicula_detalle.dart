0b2b0e6a102ddf97f7a7001d6a7c8d0893b90a31
Ivan Gonzalez <ivan@iconos.mx>
2021-03-06 11:43:44 -0800

    Desp;egando detalle pelicula

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -16,6 +16,7 @@ class PeliculaDetalle extends StatelessWidget {
               delegate: SliverChildListDelegate([
             SizedBox(height: 10.0),
             _posterTitulo(context, pelicula),
+            _description(pelicula),
           ])),
         ],
       ),
@@ -46,33 +47,40 @@ class PeliculaDetalle extends StatelessWidget {
   Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image(
-              image: NetworkImage(pelicula.getPosterImg()),
-              height: 150.0,
-            ),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image(
+            image: NetworkImage(pelicula.getPosterImg()),
+            height: 150.0,
           ),
-          SizedBox(width: 20.0),
-          Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(pelicula.title, style: Theme.of(context).textTheme.headline6, overflow: TextOverflow.ellipsis,),
-                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subtitle2, overflow: TextOverflow.ellipsis),
-                Row(
-                  children: [
-                    Icon(Icons.star_border),
-                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subtitle2, overflow: TextOverflow.ellipsis)
-                  ]
-                ),
-              ]
-            )
-          )
-        ]
-      ),
+        ),
+        SizedBox(width: 20.0),
+        Flexible(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            pelicula.title,
+            style: Theme.of(context).textTheme.headline6,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text(pelicula.originalTitle,
+              style: Theme.of(context).textTheme.subtitle2,
+              overflow: TextOverflow.ellipsis),
+          Row(children: [
+            Icon(Icons.star_border),
+            Text(pelicula.voteAverage.toString(),
+                style: Theme.of(context).textTheme.subtitle2,
+                overflow: TextOverflow.ellipsis)
+          ]),
+        ]))
+      ]),
+    );
+  }
+
+  Widget _description(Pelicula pelicula) {
+    return Container(
+      child: Text(pelicula.overview),
     );
   }
 }
